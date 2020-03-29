9d55b1efa19e11920f6ae01abae6ac22b19bf159
alexleybourne <alexleybourne1@gmail.com>
2020-03-30 07:30:20 +1000

    Basic Styling to list

--- GoalApp/App.js
+++ GoalApp/App.js
@@ -21,7 +21,7 @@ export default function App() {
         <Button title="ADD" onPress={addGoalHandler}/>
       </View>
       <View>
-        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
+        {courseGoals.map((goal) => <View  key={goal} style={styles.listItem}><Text>{goal}</Text></View>)}
       </View>
     </View>
   );
@@ -41,8 +41,16 @@ const styles = StyleSheet.create({
 
   input: {
     width: '80%', 
-    borderColor: 'lightblue',
+    borderColor: 'blue',
     borderWidth: 1, padding: 10
   },
+
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+  }
 
 });
